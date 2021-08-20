972f4266a3b55913f7dd7aa56c65dfd21d0d10cd
sergio <sergio3996@gmail.com>
2021-08-19 23:47:20 -0300

    login y logout

--- red_bicicletas/app.js
+++ red_bicicletas/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const passport = require('./config/passport');
+const session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usuariosRouter = require('./routes/usuarios');
 var usersRouter = require('./routes/users');
@@ -12,7 +15,17 @@ var bicicletasAPIRouter = require('./routes/api/bicicletas');
 var usuariosAPIRouter = require('./routes/api/usuarios');
 var tokenRouter = require('./routes/token');
 
+const store = new session.MemoryStore;
+
 var app = express();
+
+app.use(session({
+  cookie: { maxAge: 240 * 60 * 60 * 1000 },
+  store: store,
+  saveUninitialized: true,
+  resave: 'true',
+  secret: 'red_bicis_!!!***!""!""!""!"".1234'
+}));
 
 var mongoose = require('mongoose');
 
@@ -30,11 +43,77 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/login', function(req, res){
+  res.render('session/login');
+});
+
+app.post('/login', function(req,res,next){
+  passport.authenticate('local', function(err, usuario, info){
+    if (err) return next(err);
+    if (!usuario) return res.render('session/login', {info});
+    req.login(usuario,function(err){
+      if (err) return next(err);
+      return res.redirect('/');
+    });
+  })(req, res, next);
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('/forgotPassword', function(req,res){
+  res.render('session/forgotPassword');
+});
+
+app.post('/forgotPassword', function(req, res, next){
+  Usuario.findOne({ email: req.body.email }, function(err, usuario) {
+    if(!usuario) return res.render('session/forgotPassword', { info: { message: 'No existe el email para un usuario existente.' }});
+    usuario.resetPassword(function(err){
+      if(err) return next(err);
+      console.log('session/forgotPasswordMessage');
+    })
+    res.render('session/forgotPasswordMessage');
+  })
+});
+
+app.get('/resetPassword/:token', function(req,res,next){
+  Token.findOne({ token: req.params.token }, function (err, token){
+    if (!token) return res.status(400).send({ msg: 'No existe un usuario asociado al token. Verifique que su token no haya expirado.' });
+    
+    Usuario.findById(token._userId, function (err, usuario){
+      if (!usuario) return res.status(400).send({ msg: 'No existe un usuario asociado al token. '});
+      res.render('session/resetPassword', { errors: {}, usuario: usuario});
+    });
+  });
+});
+
+app.post('/resetPassword', function(req, res){
+  if (req.body.password != req.body.confirm_password) {
+    res.render('session/resetPassword', {errors: {confirm_password: {message: 'No coincide con el password ingresado'}},
+    usuario: new Usuario({email: req.body.email})});
+    return;
+  }
+  Usuario.findOne({ email: req.body.email }, function (err, usuario) {
+    usuario.password = req.body.password;
+    usuario.save(function(err){
+      if (err) {
+        res.render('session/resetPassword', { errors: err.errors, usuario: new Usuario({email: req.body.email})});
+      }else{
+        res.redirect('/login');
+      }
+    });
+  });
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/bicicletas', biciletasRouter);
+app.use('/bicicletas', loggedIn, biciletasRouter);
 app.use('/api/bicicletas', bicicletasAPIRouter);
 app.use('/api/usuarios', usuariosAPIRouter);
 app.use('/usuarios', usuariosRouter);
@@ -56,5 +135,15 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//funcion para saber si es logged
+function loggedIn(req, res, next) {
+  if (req.user){
+    next();
+  }else{
+    console.log('Usuario sin loguarse');
+    res.redirect('/login');
+  }
+};
 
 module.exports = app;
--- red_bicicletas/models/usuario.js
+++ red_bicicletas/models/usuario.js
@@ -81,4 +81,27 @@ usuarioSchema.methods.enviar_email_bienvenida = function(cb) {
     });
 }
 
+usuarioSchema.methods.resetPassword = function(cb) {
+    const token = new Token({_userId: this.id, token: crypto.randomBytes(16).toString('hex')});
+    const email_destination = this.email;
+    token.save(function (err) {
+        if (err) { return cb(err); }
+
+        const mailOptions = {
+            from: 'no-reply@redbicicletas.com',
+            to: email_destination,
+            subject: 'Reseteo de password de cuenta ',
+            text: 'Hola,\n\n' + 'Por favor, para resetear el password de su cuenta haga click en este link: \n' + ' http://localhost:5001' + '\/resetPassword\/' + token.token + '.\n'
+        };
+        
+        mailer.sendMail(mailOptions, function (err){
+            if (err) { return cb(err); }
+
+            console.log('Se envio un email para resetear el password a: ' +  email_destination + '.');
+        });
+        
+        cb(null);
+    });
+}
+
 module.exports = mongoose.model('Usuario', usuarioSchema);
